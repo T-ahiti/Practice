92339a21a490129b9e91b0ab4096bc7429afe2c3
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-10-24 18:57:31 -0400

    code for discount

--- 2/ConsoleApp1/Program.cs
+++ 2/ConsoleApp1/Program.cs
@@ -1,8 +1,24 @@
 ﻿
 using System;
 
-public static class Program{
+public static class MovieDiscount{
     public static void Main(){
-        Console.WriteLine("My name is tahiti");
+        int age;
+        char rating; 
+        const int CHILD_AGE = 12;
+        const int SENIOR_AGE = 65;
+        Console.WriteLine("When age is {0} or >=65 and rating is {1}", CHILD_AGE, SENIOR_AGE);
+        
+        Console.WriteLine("Enter age:");
+        age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter rating:");
+        rating = Convert.ToChar(Console.ReadLine());
+
+        if((age <= CHILD_AGE || age >= SENIOR_AGE)&& rating == 'G')
+            Console.WriteLine("Discount applies");
+        else 
+            Console.WriteLine("Full Price");
+        Console.ReadLine();
     }
 }
--- 2/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ 2/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c67bcad674c22433b63ad15e72dfb00abd1ca552")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42dcc0803c090987deee3e81ad33a0493a9a7abf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
