eb8ac31d4dc7c2be7e24286a9b6f40bcab46d268
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-10-24 22:58:53 -0400

    switch added

---
+++ 4/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+public static class yearName
+{
+   public static void Main(){
+
+         int year;
+         Console.WriteLine("Enter year:");
+         year = Convert.ToInt32(Console.ReadLine());
+
+      switch(year){
+            case 1:
+            Console.WriteLine("Freshman");
+            break;
+      
+            case 2:
+            Console.WriteLine("second year");
+            break;
+      
+
+            case 3:
+            Console.WriteLine("third year");
+            break;
+     
+            case 4:
+            Console.WriteLine("senior");
+            break;
+      
+            default:
+            Console.WriteLine("Invalid year");
+            break;
+        }
+
+   }
+   
+}
