84237102958d40d8fe903bd3d9e1b98e64056e12
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-10-25 02:52:18 -0400

    bank balance math

---
+++ 7/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Threading.Tasks.Dataflow;
+
+public static class BankBal{
+    public static void Main(){
+        double BankBal = 1000;
+        const double INT_RATE = 0.04;
+        char response;
+
+        Console.WriteLine("Do you want to see your balance? Y or N:");
+        response = Convert.ToChar(Console.ReadLine());
+
+        while(response == 'Y'){
+            Console.WriteLine("your balance is {0:C}", BankBal);
+            BankBal = BankBal + BankBal * INT_RATE;
+
+            Console.WriteLine("Do you want to see next years balance? Y or N:");
+             response = Convert.ToChar(Console.ReadLine());
+        }
+          Console.WriteLine("HAVE A NICE DAY");
+          Console.ReadLine();
+
+    }
+}
