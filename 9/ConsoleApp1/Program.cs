cc1df92db8a8cfe12a9ab1e9fa92a37d426cbc9e
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-10-25 13:37:33 -0400

    completed average

---
+++ 9/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+public static class AverageMarks
+{
+    public static void Main(){
+        int numbers, num;
+        double average, total=0;
+
+        Console.WriteLine("Enter the number of marks (+ve) =>");
+        numbers = Convert.ToInt32(Console.ReadLine());
+
+    for(int i=0; i<numbers; ++i){
+        Console.WriteLine("Enter a mark =>");
+        num = Convert.ToInt32(Console.ReadLine());
+        total = total + num;
+    }
+     average = total / (1.0 * numbers);
+     Console.WriteLine("the average is {0}", average);
+    }
+}
