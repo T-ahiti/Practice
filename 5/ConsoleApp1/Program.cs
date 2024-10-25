f43bc710d62cd748dbfd47abe4905d21eece710f
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-10-24 23:45:30 -0400

    code for food grade

---
+++ 5/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+public static class PetFood
+{
+    public static void Main()
+    {
+        char foodGrade;
+
+        Console.WriteLine("Enter the grade of your food:");
+        foodGrade = Convert.ToChar(Console.ReadLine());
+
+        switch(foodGrade){
+            case 'A':
+            Console.WriteLine("the food is high quality");
+            break;
+
+            case 'B':
+            Console.WriteLine("this food is medium quality");
+            break;
+
+            case 'C':
+            Console.WriteLine("this is low quality food");
+            break;
+
+            default:
+            Console.WriteLine("this grade is invalid");
+            break;
+        }
+    }
+}
