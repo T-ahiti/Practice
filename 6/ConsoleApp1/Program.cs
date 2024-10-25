31a825077700ba51305047b22a95be0d5af2f3b6
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-10-25 02:00:21 -0400

    while loop added

---
+++ 6/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+public static class FourHellos{
+    public static void Main(){
+        int number =1;
+        const int LIMIT = 5;
+        while(number < LIMIT){
+        Console.WriteLine("Hello");
+        number++ ;
+        }
+    }
+}
