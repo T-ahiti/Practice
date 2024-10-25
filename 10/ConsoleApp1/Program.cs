aa3e97f97edb2d34c70ff05f9ff4d46cc325a95b
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-10-25 15:46:35 -0400

    while loop

---
+++ 10/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Globalization;
+
+public static class whileLoop
+{
+    public static void Main(){
+        int numbers;
+
+        Console.WriteLine("Enter how many numbers you want to get average of:");
+        numbers= Convert.ToInt32(Console.ReadLine());
+
+
+
+    }
+}
